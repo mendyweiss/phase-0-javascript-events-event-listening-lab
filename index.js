c70d49c2917a3c44dfe069addd319cce74dc8b62
Mendy <mendyw94@gmail.com>
2021-12-10 02:39:26 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+    input.style.background = 'red';
+    input.addEventListener('onclick', alert('This is fun!'));
+    ;
 }
+addingEventListener();
+
+
+
+/*const input = document.getElementById('input');
+input.addEventListener('click', function() {
+    alert('I was clicked!');
+})*/
+
